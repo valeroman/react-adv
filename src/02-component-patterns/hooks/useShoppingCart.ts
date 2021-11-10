14b4cebf7e3422f79c455bff61542e1469cc47d3
Roman Valero <valeroman@gmail.com>
2021-11-10 16:23:56 -0500

    Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,31 +9,17 @@ export const useShoppingCart = () => {
 
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            if (count === 0) {
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                // console.log({toDelete});
+                return rest;
             }
 
-            // Borrar Producto
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
-
-            // if (count === 0) {
-
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //     // console.log({toDelete});
-            //     return rest;
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: { ...product, count }
-            // }
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
+            }
         });
     }
 
